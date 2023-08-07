c8626394d54d89ce996ecf8e41b496bad6078f48
f1ninh0 <134070842+f1ninh0@users.noreply.github.com>
2023-08-07 16:06:46 -0300

    add project

---
+++ matrizes/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace testeNuria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool encerrar = false;
+            while (!encerrar) {
+
+                Console.WriteLine("Escolha entre as opcoes:");
+                Console.WriteLine("1 - Para inverter matriz");
+                Console.WriteLine("2 - Para encontrar sub matriz");
+                Console.WriteLine("0 - Para sair");
+
+                var opcao = 0;
+                while (!int.TryParse(Console.ReadLine(), out opcao))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opcao invalida");
+                    Console.WriteLine("Escolha entre as opcoes:");
+                    Console.WriteLine("1 - Para inverter matriz");
+                    Console.WriteLine("2 - Para encontrar sub matriz");
+                    Console.WriteLine("0 - Para sair");
+                }
+
+                switch (opcao)
+                {
+                    case 1:
+                        {
+                            Functions.InverteMatriz();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Functions.EncontrarSubMatriz();
+                            break;
+                        }
+                    case 0:
+                    default:
+                        {
+                            encerrar = true;
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
